e5cb973bafbb8e851a19f0ad02ec52f5b423b6f1
Spencer <spencer.p.816@gmail.com>
2016-12-02 01:36:20 -0800

    Add basic react components for each page
    (ref #12)

---
+++ public/scripts/loginPage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+class LoginPage extends React.Component {
+
+    render() {
+
+        return(
+
+            <div></div>
+
+        );
+    }
+}
+
+var loginWrapper = document.getElementById("login-wrapper");
+ReactDOM.render(<LoginPage />, loginWrapper);
---
+++ public/scripts/profilePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+class ProfilePage extends React.Component {
+
+    render() {
+
+        return(
+
+            <div></div>
+
+        );
+    }
+}
+
+var profileWrapper = document.getElementById("profile-wrapper");
+ReactDOM.render(<ProfilePage />, profileWrapper);
