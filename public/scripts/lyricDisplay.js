344588772bb5918f9ee59b89ea3cd2ed610ddd56
Spencer <spencer.p.816@gmail.com>
2016-12-05 14:08:10 -0800

    Create Component to display lyrics for a song
    (ref #3)

---
+++ public/scripts/lyricDisplay.js
@@ -0,0 +1,12 @@
+"use strict";
+
+class LyricDisplay extends React.Component {
+
+    render() {
+        return (
+            <div className="lyric-display">
+                
+            </div>
+        );
+    }
+}
