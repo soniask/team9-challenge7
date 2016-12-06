edf7690ddebbe91ad1674ab4de0a3544358ca668
cazadori <dubeh@uw.edu>
2016-12-05 22:43:46 -0800

    added login pages. Messages also displayed on error.

--- public/scripts/loginPage.js
+++ public/scripts/loginPage.js
@@ -1,14 +1,118 @@
 "use strict";
 
 class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            signupError: false,
+            loginError: false,
+            error: ""
+        }
+    }
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user) {
+                window.location.href="search.html";
+            }
+        });
+    }
 
     render() {
 
         return(
 
-            <div></div>
+            <div>
+                <div className="col col-sm-6">
+                    <SignupForm onSubmit={(name, email, password, passwordConfirm) => this.createUser(name, email, password, passwordConfirm)}/>
+                    {
+                        this.state.signupError ? (
+
+                            <div ref="signupError" className="alert alert-danger" role="alert">{this.state.error}</div>
+
+                        ) : null
+                    }
+                </div>
+                <div className="col col-sm-6">
+                    <LoginForm onSubmit={(email, password) => this.loginUser(email, password)}/>
+                    {
+                        this.state.loginError ? (
+
+                            <div ref="loginError" className="alert alert-danger" role="alert">{this.state.error}</div>
+                            
+                        ) : null
+                    }
+                </div>
+            </div>
 
         );
+    }
+
+    createUser(name, email, password, passwordConfirm) {
+
+        this.setState({
+            signupError: false,
+            loginError: false
+        })
+
+        if(password == passwordConfirm) {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((user) => {
+
+                user.updateProfile({
+                    displayName: name
+                }).then(() => {
+
+                }).catch((error) => {
+                    this.setState({
+                        signupError: true,
+                        error: error.message
+                    })
+                });
+
+                user.sendEmailVerification()
+                .then(() => {
+                    window.location.href = "search.html";
+                }).catch((error) => {
+                    this.setState({
+                        signupError: true,
+                        error: error.message
+                    })
+                })
+            })
+            .catch((error) => {
+                this.setState({
+                    signupError: true,
+                    error: error.message
+                })
+            })
+        } else {
+            var errorMessage = "Passwords do not match";
+            this.setState({
+                signupError: true,
+                error: errorMessage
+            })
+        }
+    }
+
+    loginUser(email, password) {
+
+        this.setState({
+            signupError: false,
+            loginError: false
+        })
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(() => {
+            window.location.href = "search.html";
+        })
+        .catch((error) => {
+            this.setState({
+                loginError: true,
+                error: error.message
+            })
+        })
     }
 }
 
