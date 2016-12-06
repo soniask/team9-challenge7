0fe7c4b230f6380b08296f598fa6b96fc78eda72
Spencer <spencer.p.816@gmail.com>
2016-12-05 17:16:46 -0800

    Add functionality to display song lyrics, title, and album
    (ref #3)

--- public/scripts/lyricDisplay.js
+++ public/scripts/lyricDisplay.js
@@ -3,10 +3,40 @@
 class LyricDisplay extends React.Component {
 
     render() {
+
+        var lyrics = this.props.lyrics;
+        var title = this.props.title;
+        var album = this.props.album;
+
         return (
-            <div className="lyric-display">
+            <div className="song-display">
+                <h2>{this.toTitleFormat(title)}</h2>
+                <h3>From the album <em>{this.toTitleFormat(album)}</em></h3>
+
+                <div className="lyric-display">
+                    {
+                        this.props.lyrics.map((line, index) => (
+                            <div
+                                className="lyric-line"
+                                key={album + "-" + title + "-" + index}
+                                id={album + "-" + title + "-" + index}
+                            >
+                                {line}
+                            </div>   
+                        ))
+                    }
+                </div>
                 
             </div>
         );
+    }
+
+    // Converts a string of format "this_is_a_title" to "This Is A Title", returning a new string
+    toTitleFormat(text) {
+        var newText = text
+            .replace(/_/g, " ")
+            .replace(/(?: |\b)(\w)/g, function(key) { return key.toUpperCase() });
+
+        return(newText);
     }
 }
