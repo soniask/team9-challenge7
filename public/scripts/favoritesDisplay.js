aa8b7bce82341cf8b2022c77e1516eded19f5fb9
Sean Yeager <Sean Yeager>
2016-12-07 12:19:24 -0800

    Don't merge this in yet.  Unknown FB problem producing errors

---
+++ public/scripts/favoritesDisplay.js
@@ -0,0 +1,26 @@
+"use strict";
+
+class FavoritesDisplay extends React.Component {
+
+    
+    render() {
+        var favorites = this.props.favorites;    
+
+       return (
+            <div>
+                 <h3>Your Favorite Lyrics</h3>
+                     <ul>
+                         {
+                             this.state.favorites.map((favorites)=> {
+                                 return <li key={favorites}>
+                                     <a href="#" onClick={(e) => this.onSavedClick(e, favorites)}>
+                                         {favorites}
+                                     </a>
+                                 </li>
+                             })
+                         }
+                     </ul>
+            </div>
+         )
+     }          
+ }
--- public/scripts/profilePage.js
+++ public/scripts/profilePage.js
@@ -3,14 +3,46 @@
 class ProfilePage extends React.Component {
 
     render() {
-
+        var database = firebase.database();
+        var user = firebase.User();
+        this.setState(favorites= database.ref( user.uid + '/{favorites}'))
         return(
 
-            <div></div>
+            <div>
+                <Header/>
+                <FavoritesDisplay
+                favorites={this.state.favorites}
+                
+                />
+
+            
+            </div>
 
         );
     }
+
+
+    
 }
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // Data fetching
+
+        
+        
+        
+
+        // favorites.on('child_added', function(data) {
+        //     var id = data.key;
+        //     var favorite = data.val();
+        // });
+    
+    } else {
+        //redirect to index.html
+        window.location.href= "index.html";
+    }
+})
 
 var profileWrapper = document.getElementById("profile-wrapper");
 ReactDOM.render(<ProfilePage />, profileWrapper);
