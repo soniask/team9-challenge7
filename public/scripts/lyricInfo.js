ba69e671aef475d40e8df4dcc4ccc0380d439f89
Spencer <spencer.p.816@gmail.com>
2016-12-06 18:50:16 -0800

    Create basic layout and logic for `LyricInfo`
    (ref #19)

--- public/scripts/lyricInfo.js
+++ public/scripts/lyricInfo.js
@@ -4,11 +4,51 @@ class LyricInfo extends React.Component {
 
     render() {
 
+        var user = firebase.auth().currentUser;
+
         return(
 
-            <div>
+            <div className="lyric-info">
+
+                <div className="chosen-line">
+                    {this.props.line}
+                </div>
+
+                {
+                    user
+                        ?
+                            <div>
+                                <div className="favorite-list">
+                                    <ul>
+                                        <li>hi</li>
+                                        <li>hi</li>
+                                        <li>hi</li>
+                                        <li>hi</li>
+                                    </ul>
+                                </div>
+
+                                <button 
+                                    type="button"
+                                    className="btn btn-primary btn-lg"
+                                    onClick={(e) => this.addfavorite(e)}
+                                >
+                                    Favorite line
+                                </button>
+
+                            </div>
+                        : 
+                            <div className="alert alert-info" role="alert">
+                                You must be logged in to see other user's favorites and favorite your own lyrics
+                            </div>
+                }
             
             </div>
         );
+    }
+
+    addfavorite(e) {
+        e.preventDefault();
+
+
     }
 }
